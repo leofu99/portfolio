39866580c6a4d995367e60629f9f11207ef74288
leofu99 <leonardo.fuentes@udea.edu.co>
2021-08-02 23:55:34 -0500

    Header sidebar y about

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { WorksComponent } from './works/works.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    WorksComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
